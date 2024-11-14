43992664f48fb5a500395281546e4e9b4adcc8dc
shubinov <shubinov@gmail.com>
2024-11-14 13:42:53 -0500

    update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,3 +104,18 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+
+    def test_read_product(self):
+        """ It should Read a Product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+
+        found_product = Product.find(product.id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description, product.description)
+        self.assertEqual(found_product.price, product.price)
+        #self.assertEqual(found_product.available, product.available)
+        #self.assertEqual(found_product.category, product.category)
